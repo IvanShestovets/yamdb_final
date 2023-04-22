746e52780186114a91eafbe6fd0e5e5c1c32cd9a
Иван Шестовец <shestovetsivan@gmail.com>
2023-04-23 00:54:58 +0300

    My commit

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Title, Comment, Review, Genre, Category
+from .models import Category, Comment, Genre, Review, Title
 
 
 class TitleAdmin(admin.ModelAdmin):
